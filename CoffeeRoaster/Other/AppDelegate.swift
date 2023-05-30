4956acdd4aa5a817c439223668b04f39cf447b74
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-05-30 22:04:46 +0300

    - Handle Notifications

--- CoffeeRoaster/Other/AppDelegate.swift
+++ CoffeeRoaster/Other/AppDelegate.swift
@@ -10,18 +10,51 @@ import Firebase
 import FirebaseCore
 import UserNotifications
 
+
+
+
+
+//MARK: UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.sound, .alert])
+        print(#function)
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(#function)
+    }
+    
+}
+
+
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        // User Notification Authorization
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted , error) in
-            if granted {
-                print("User gave permissions for local notifications")
+        
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { allow, error in
+            // check user's permission
+            guard allow else { return }
+            // check what exactly user accept
+            self.notificationCenter.getNotificationSettings { settings in
+                print(settings)
+                // if all of settings allowed, the guard statement let us go bellow
+                guard settings.authorizationStatus == .authorized else { return }
             }
+            
         }
+        notificationCenter.delegate = self
+        
         return true
     }
 
